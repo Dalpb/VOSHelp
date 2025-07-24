cab914057b1c44e1eca2b123480b9f2f1535ae14
Diego Palomino <dpalomino032002@gmail.com>
2025-07-24 18:11:56 -0500

    updating layout

---
+++ src/modules/vgmemory/pages/VGmemoryview.tsx
@@ -0,0 +1,19 @@
+import { Flex, Layout } from "antd";
+import styles from "./vgmemoryview.module.css";
+
+const VGmemoryview = () => {
+  const { Header, Footer, Content } = Layout;
+  return (
+    <Layout>
+      <Header>VGMemory</Header>
+      <Content>
+        <Flex gap="middle">
+          <section className={styles.canva_section}></section>
+          <section className={styles.code_section}></section>
+        </Flex>
+      </Content>
+      <Footer></Footer>
+    </Layout>
+  );
+};
+export default VGmemoryview;
