a8345ca2ca7761d6dd3cde3f423d7a1c59bc38d2
Diego Palomino <dpalomino032002@gmail.com>
2025-07-24 18:47:30 -0500

    cambio

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:[
+      {
+        find: '@vgmemory',
+        replacement: path.resolve(path.join(__dirname,'/src/modules/vgmemory'))
+      }
+    ]
+  }
 })
